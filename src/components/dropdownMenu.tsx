8b6fe74c45cb6a01b92e6549697c62e7314f8609
sumit298 <sumitsinhassss@outlook.com>
2023-09-19 11:57:15 +0530

    layout fix 2 dropdown

--- src/components/dropdownMenu.tsx
+++ src/components/dropdownMenu.tsx
@@ -2,10 +2,9 @@ import CustomDropdown from "./dropdown";
 
 const DropDownMenu = () => {
   return (
-    <div className="grid grid-cols-2 m-4 gap-y-4 opacity-75 py-8 px-2 w-[95%] rounded-xl bg-white">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 m-4 gap-y-4 opacity-75 py-8 px-2 w-full sm:w-[95%] rounded-xl bg-white">
       <CustomDropdown title="Class" options={["1", "2", "3", "4", "5", "6", "7", "8", "9", "Add Class"]} />
-      <CustomDropdown title="Subject" options={["Science", "Mathematics", 
-    "History", "Add Subject"]} />
+      <CustomDropdown title="Subject" options={["Science", "Mathematics", "History", "Add Subject"]} />
       <CustomDropdown title="Topic" options={["Biology", "Chemistry", "Physics", "Add topic"]} />
       <CustomDropdown title="Sub topic" options={["Genetics", "Ecology", "Human Anatomy", "Add Subtopic"]} />
       <CustomDropdown title="Level" options={["Beginner", "Intermediate", "Advance"]} />
